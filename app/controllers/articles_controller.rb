27e43b5d343f4ab052d4acb6913233c928a5d7e1
Edil Turdumambetov <edil13@mail.ru>
2023-02-05 23:40:31 +0300

    Added the new page with new article form, Added action #create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article
+        @article.save
+        #redirect_to article_path(@article)
+        #the short version for the above line is:
+        redirect_to @article
+    end
 end
